2273432990f0a98ab1c8510a0cab3c12196ad881
IlkoAdamov <ilko.z.adamov@gmail.com>
2026-06-15 13:09:16 +0300

    adding turkish, german, french and russian lang

--- i18n.ts
+++ i18n.ts
@@ -4,14 +4,21 @@ import { getLocales } from "expo-localization";
 
 import en from "./locales/en.json";
 import bg from "./locales/bg.json";
+import tr from "./locales/tr.json";
+import ru from "./locales/ru.json";
+import de from "./locales/de.json";
+import fr from "./locales/fr.json";
 
-//const deviceLanguage = getLocales()[0]?.languageCode ?? "en";
-const deviceLanguage = "bg";
+const deviceLanguage = getLocales()[0]?.languageCode ?? "en";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     bg: { translation: bg },
+    tr: { translation: tr },
+    ru: { translation: ru },
+    de: { translation: de },
+    fr: { translation: fr },
   },
   lng: deviceLanguage,
   fallbackLng: "en",
